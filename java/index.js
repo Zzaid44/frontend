5c402b7faf5eea4c946b37ba3861e4f71e29e362
Zzaid44 <zaidbanisakher286@gmail.com>
2025-11-26 23:39:42 +0300

    midporject

---
+++ java/index.js
@@ -0,0 +1,38 @@
+// alert('js')
+// var username='zaid'
+// var age=20
+// var isstu=true
+// var graed=[20,65,55]
+// var mark=null
+// var my
+// var userdate={nam: 'aya', age:20}
+
+//class:hoverrrrrrrrrrrrrrrrrr
+// console.log(username)
+// console.log(age)
+// console.log(isstu)
+// console.log(graed[2])
+// console.log(mark)
+// console.log(my)
+// console.log(userdate)
+// let x=5
+// console.log(x)
+//const j=8مابتتغير erorr
+// console.log(j)
+//=== بتقارن كلشي
+// == بتقارن ال الرقم بدون الداتا تايب
+// var navbar=document.getElementById('navbar')
+//   navbar.style.backgroundColor='yellow'
+//   navbar.style.borderRadius='40px'
+//   var link=document.getElementsByTagName('a')[0]
+//   console.log(link)
+//   link.style.color=('red')
+
+  var z=10
+  if(z ==='10'){
+    console.log('z is equal')
+  }
+else if(z ==10){
+    console.log('z is not equal 10')
+
+}
